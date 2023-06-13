c2cbcb8d3e42b047a1f2f9efc5820a0b425069ae
AliAsghar01 <104501434+AliAsghar01@users.noreply.github.com>
2023-06-13 20:09:30 +0200

    workflow test5

--- project/tests.sh
+++ project/tests.sh
@@ -3,7 +3,7 @@
 
 echo "pipeline fle executed"
 # Execute your pipeline
-chmod +x python pipline.py
+python -m project/data/pipline.py -v
 
 # Validate the output file(s)
 if [ -f "traffic_violation.sqlite" ]; then
@@ -13,5 +13,5 @@ else
 fi
 
 echo " test file excuted"
-chmod +x python test.py
+python -m project/test.py -v
 
